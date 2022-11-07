0ba7a9d0b5f1cf300aa6cd51b3693c30d4390fec
majapavlovic <maja.pavlovic2703@gmail.com>
2022-11-07 23:12:20 +0100

    Created controller, resource and route for User

--- flashcards/routes/api.php
+++ flashcards/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/flashcards/users', [UserController::class, 'index']);
+Route::get('/flashcards/users/{user_id}', [UserController::class, 'show']);
+
