829c95e002b925393c0fdef17197a24e6d620935
majapavlovic <maja.pavlovic2703@gmail.com>
2022-11-07 19:51:06 +0100

    created migrations

--- flashcards/database/migrations/2022_11_07_012028_create_questions_table.php
+++ flashcards/database/migrations/2022_11_07_012028_create_questions_table.php
@@ -13,15 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role'); //admin or ordinary user
-            $table->rememberToken();
+            $table->text('question');
             $table->timestamps();
+            $table->foreignId('image_id')->nullable();
+            $table->foreignId('category_id');
+            $table->foreignId('user_id');
         });
     }
 
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('questions');
     }
 };
--- flashcards/database/migrations/2022_11_07_165413_create_images_table.php
+++ flashcards/database/migrations/2022_11_07_165413_create_images_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role'); //admin or ordinary user
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('file_path');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('images');
     }
 };
