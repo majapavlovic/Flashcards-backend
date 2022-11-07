7234bb65edd8ad7b1a3cd8f9404a20b7fc09f987
majapavlovic <maja.pavlovic2703@gmail.com>
2022-11-07 19:51:31 +0100

    created models

---
+++ flashcards/app/Models/Answer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['answer','question_id','image_id'];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function image() {
+        return $this->belongsTo(Image::class);
+    }
+}
